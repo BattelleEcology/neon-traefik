f4b27e5d896d629a9ed13a0fedbc96d993901052
jsampson <jsampson@battelleecology.org>
2025-06-27 08:56:58 -0600

    neon-traefik github actions

--- .neon/docker-build.sh
+++ .neon/docker-build.sh
@@ -1,11 +1,25 @@
-#!/bin/bash
-LOC=$(pwd)
-cd ..
+#!/usr/bin/env bash
+
+set -e
 
 # Default build target
 GOOS='linux'
 GOARCH='amd64'
 
-docker buildx build --load --platform=${GOOS}/${GOARCH} -t us-central1-docker.pkg.dev/neon-shared-service/bei/neon-traefik --file Dockerfile .
+if [ -n "$1" ]; then
+    echo "Version: $1"
+    TAG="$1"
+else
+    echo "No tag set, exiting"
+    exit
+fi
 
-cd $LOC
+if [ -n "$2" ]; then
+    echo "Image: $2"
+    IMAGE_TAG="$2"
+else
+    echo "No image tag set, exiting"
+    exit
+fi
+
+docker buildx build --load --platform=${GOOS}/${GOARCH} --build-arg TAG=$TAG -t $IMAGE_TAG --file Dockerfile .
